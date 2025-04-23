ae9d87e4b10a5dfc9787c21abc2c5b0a9f409c2c
beshoykhalel <beshoy.khalel2019@gmail.com>
2025-04-23 22:59:22 +0200

    `Update grayColor in AppColors to use Color.fromARGB`

--- lib/core/styling/app_colors.dart
+++ lib/core/styling/app_colors.dart
@@ -4,7 +4,7 @@ class AppColors {
   static Color primaryColor = const Color(0xff3669C9);
   static Color scondryColor = const Color(0xff8391A1);
   static Color balckColor = const Color(0xff1F2C37);
-  static Color grayColor = const Color(0xff9CA4AB);
+  static Color grayColor = const Color.fromARGB(15, 175, 170, 157);
   static Color anotherColor = const Color(0xff303A6E);
   static Color whiteColor = Colors.white;
   static Color redColor = Colors.red;
