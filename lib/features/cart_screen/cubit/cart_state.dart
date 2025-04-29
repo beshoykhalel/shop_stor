885bfb4d58ff8493a606620c5dd727a34c35b6fe
beshoykhalel <beshoy.khalel2019@gmail.com>
2025-04-29 13:23:23 +0300

    add cart_State and cart cart_cubit

---
+++ lib/features/cart_screen/cubit/cart_state.dart
@@ -0,0 +1,31 @@
+import 'package:shop_stor/features/cart_screen/models/cart_model.dart';
+
+abstract class CartState {}
+
+class LoadindCarts extends CartState {}
+
+class SuccessGettingCarts extends CartState {
+  final CartModel cart;
+
+  SuccessGettingCarts(this.cart);
+}
+
+class ErrorGettingCarts extends CartState {
+  final String message;
+
+  ErrorGettingCarts(this.message);
+}
+
+class AddingToCarts extends CartState {}
+
+class SuccessAddingToCarts extends CartState {
+  final CartModel cart;
+
+  SuccessAddingToCarts(this.cart);
+}
+
+class ErrorAddingToCarts extends CartState {
+  final String message;
+
+  ErrorAddingToCarts(this.message);
+}
