cf4ba1368fbd9496303a5a09bfa7b98bc0744079
beshoykhalel <beshoy.khalel2019@gmail.com>
2025-04-29 10:56:19 +0300

    `Removed import of app_assets package from prodect_screen.dart`

--- lib/features/prodect_screen/prodect_screen.dart
+++ lib/features/prodect_screen/prodect_screen.dart
@@ -1,7 +1,6 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:shop_stor/core/styling/app_assets.dart';
 import 'package:shop_stor/core/styling/app_colors.dart';
 import 'package:shop_stor/core/styling/app_styles.dart';
 import 'package:shop_stor/core/widgets/primary_button_widget.dart';
